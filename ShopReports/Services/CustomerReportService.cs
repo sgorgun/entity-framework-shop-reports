0b956995c8ecd53120684da790fe00ea69eb563e
sgorgun <sergei.gorgun@gmail.com>
2024-03-21 06:02:20 +0200

    Problem with linking customers and persons tables. There may be an error in the diagram.

--- ShopReports/Services/CustomerReportService.cs
+++ ShopReports/Services/CustomerReportService.cs
@@ -15,8 +15,42 @@ namespace ShopReports.Services
 
         public CustomerSalesRevenueReport GetCustomerSalesRevenueReport()
         {
-            // TODO Implement the service method.
-            throw new NotImplementedException();
+            var query = this.shopContext.Customers
+                .Join(
+                    this.shopContext.Orders,
+                    c => c.Id,
+                    co => co.CustomerId,
+                    (c, co) => new
+                    {
+                        CustomerId = c.Id,
+                        PersonFirstName = c.Person.FirstName,
+                        PersonLastName = c.Person.LastName,
+                        OrderId = co.Id,
+                    })
+                .Join(
+                    this.shopContext.Persons,
+                    c => c.CustomerId,
+                    p => p.Id,
+                    (c, p) => new
+                    {
+                        CustomerId = c.CustomerId,
+                        PersonFirstName = c.PersonFirstName,
+                        PersonLastName = c.PersonFirstName,
+                    })
+                .Join(
+                    this.shopContext.OrderDetails,
+                    co => co.CustomerId,
+                    od => od.OrderId,
+                    (co, od) => new CustomerSalesRevenueReportLine
+                    {
+                        CustomerId = co.CustomerId,
+                        PersonFirstName = co.PersonFirstName,
+                        PersonLastName = co.PersonLastName,
+                        SalesRevenue = od.PriceWithDiscount,
+                    }).ToList();
+
+            var report = new CustomerSalesRevenueReport(query, DateTime.Now);
+            return report;
         }
     }
 }
--- ShopReports/Services/ProductReportService.cs
+++ ShopReports/Services/ProductReportService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
 using ShopReports.Models;
 using ShopReports.Reports;
 
@@ -15,26 +17,133 @@ namespace ShopReports.Services
 
         public ProductCategoryReport GetProductCategoryReport()
         {
-            // TODO Implement the service method.
-            throw new NotImplementedException();
+            var query = this.shopContext.Categories
+                .OrderBy(pc => pc.Name)
+                .Select(pc => new ProductCategoryReportLine
+                {
+                    CategoryId = pc.Id,
+                    CategoryName = pc.Name,
+                })
+                .ToList();
+
+            var report = new ProductCategoryReport(query, DateTime.Now);
+
+            return report;
         }
 
         public ProductReport GetProductReport()
         {
-            // TODO Implement the service method.
-            throw new NotImplementedException();
+            var query = this.shopContext.Titles
+                .OrderByDescending(p => p.Title)
+                .Join(
+                    this.shopContext.Products,
+                    pt => pt.Id,
+                    p => p.TitleId,
+                    (pt, p) => new
+                    {
+                        ProductId = p.Id,
+                        ProductTitle = pt.Title,
+                        Price = p.UnitPrice,
+                        ProductManufacturierId = p.ManufacturerId,
+                    })
+                .Join(
+                    this.shopContext.Manufacturers,
+                    p => p.ProductManufacturierId,
+                    m => m.Id,
+                    (p, m) => new ProductReportLine
+                    {
+                        ProductId = p.ProductId,
+                        ProductTitle = p.ProductTitle,
+                        Price = p.Price,
+                        Manufacturer = m.Name,
+                    })
+                .ToList();
+
+            var report = new ProductReport(query, DateTime.Now);
+            return report;
         }
 
         public FullProductReport GetFullProductReport()
         {
-            // TODO Implement the service method.
-            throw new NotImplementedException();
+            var query = this.shopContext.Categories
+                .Join(
+                    this.shopContext.Titles,
+                    pc => pc.Id,
+                    pt => pt.CategoryId,
+                    (pc, pt) => new
+                    {
+                        TitleId = pt.Id,
+                        CategoryId = pc.Id,
+                        CategoryName = pc.Name,
+                        ProductTitle = pt.Title,
+                    })
+                .OrderBy(p => p.ProductTitle)
+                .Join(
+                    this.shopContext.Products,
+                    p => p.TitleId,
+                    p => p.TitleId,
+                    (pt, p) => new
+                    {
+                        ProductId = p.Id,
+                        Title = pt.ProductTitle,
+                        Price = p.UnitPrice,
+                        ProductManufacturierId = p.ManufacturerId,
+                        CatId = pt.CategoryId,
+                        Category = pt.CategoryName,
+                    })
+                .Join(
+                    this.shopContext.Manufacturers,
+                    p => p.ProductManufacturierId,
+                    m => m.Id,
+                    (p, m) => new FullProductReportLine
+                    {
+                        ProductId = p.ProductId,
+                        Name = p.Title,
+                        CategoryId = p.CatId,
+                        Category = p.Category,
+                        Manufacturer = m.Name,
+                        Price = p.Price,
+                    })
+                .ToList();
+
+            var report = new FullProductReport(query, DateTime.Now);
+            return report;
         }
 
         public ProductTitleSalesRevenueReport GetProductTitleSalesRevenueReport()
         {
-            // TODO Implement the service method.
-            throw new NotImplementedException();
+            var query = this.shopContext.Titles
+                .Join(
+                    this.shopContext.Products,
+                    pt => pt.Id,
+                    sp => sp.TitleId,
+                    (pt, sp) => new
+                    {
+                        ProductTitleName = pt.Title,
+                        ProductID = sp.Id,
+                    })
+                .Join(
+                    this.shopContext.OrderDetails,
+                    sp => sp.ProductID,
+                    od => od.ProductId,
+                    (sp, od) => new ProductTitleSalesRevenueReportLine
+                    {
+                        ProductTitleName = sp.ProductTitleName,
+                        SalesRevenue = od.PriceWithDiscount,
+                        SalesAmount = od.ProductAmount,
+                    })
+                .GroupBy(od => od.ProductTitleName)
+                .Select(g => new ProductTitleSalesRevenueReportLine
+                {
+                    ProductTitleName = g.Key,
+                    SalesRevenue = g.Sum(od => od.SalesRevenue),
+                    SalesAmount = g.Sum(od => od.SalesAmount),
+                })
+                .OrderByDescending(od => od.SalesRevenue)
+                .ToList();
+
+            var report = new ProductTitleSalesRevenueReport(query, DateTime.Now);
+            return report;
         }
     }
 }
